442e776755fb81a522500b7ffb8b9735cca322c2
Naveena Ponna <Naveena@welkin-digital.com>
2024-04-18 21:32:04 +0530

    login user and updateuserinfo

--- Controller/usercontroller.js
+++ Controller/usercontroller.js
@@ -68,4 +68,58 @@ const validateopt = async (req, res) => {
       res.status(500).json({ message: 'Internal server error' });
     }
   };
-module.exports = { registerUser,validateopt};
+  const loginUser = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        return res.status(400).json({ message: 'Email is required' });
+      }
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid email ' });
+      }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid password' });
+      }
+      const token = jwt.sign({ email: user.email }, 'x-access-token', { expiresIn: '1h' });
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+  const updateUserInformation = async (req, res) => {
+    try {
+      const { location, age, workDetails } = req.body;
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(401).json({ message: 'Authorization header missing' });
+      }
+  
+      const token = authHeader.split(' ')[1];
+      if (!token) {
+        return res.status(401).json({ message: 'Token missing in Authorization header' });
+      }
+  
+      const decodedToken = jwt.verify(token, 'x-access-token');
+      const userEmail = decodedToken.email;
+      const user = await User.findOneAndUpdate(
+        { email: userEmail },
+        { $set: { location, age, workDetails, validated: true } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.status(200).json({ message: 'User information updated successfully' });
+    } catch (error) {
+      console.error(error);
+      if (error.name === 'JsonWebTokenError') {
+        return res.status(401).json({ message: 'Invalid token' });
+      }
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+module.exports = { registerUser,validateopt,loginUser,updateUserInformation};
